01537c77bf38665351cb051a28c8acd6b6634dd6
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-03-09 18:32:19 +0300

    add stdout

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123456");
+            Console.WriteLine("http://user:123456@jetbrains.com");
+        }
+    }
+}
