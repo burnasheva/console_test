81f2238923f52bd69645069194aae06e6e4e2835
Nadezhda Burnasheva <nadezhda.burnasheva@gmail.com>
2016-04-06 12:59:19 +0300

    add new project and solution

---
+++ TestSmth/TestSmth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestSmth
+{
+    public class TestSmth
+    {
+        public void PrintToConsole()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
